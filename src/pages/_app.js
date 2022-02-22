35ba4e5af4fc941514533814e871c0ff2519c1aa
Jian <8270876@gmail.com>
2022-02-22 21:35:57 +0100

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import Layout from '../layout/Layout';
+import '../styles/globals.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { PostCard, PostWidget, Categories } from '../components';
+import { getPosts } from '../services';
+import { FeaturedPost, FeaturedPosts } from '../sections';
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Wechat Moment</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={post.node.title} post={post.node} />
+          ))}
+        </div>
+
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
